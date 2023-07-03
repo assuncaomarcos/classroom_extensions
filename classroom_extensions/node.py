364c73bca8045a3cdb0cd7c072508069509bee1e
Marcos Assuncao <assuncao@acm.org>
2023-07-03 11:39:30 -0400

    Removed typing in callback description

--- classroom_extensions/node.py
+++ classroom_extensions/node.py
@@ -30,7 +30,7 @@ class NodeProcessManager(object):
         self._node_cmd = shutil.which('node')  # Try to discover full path of node command
 
     @classmethod
-    async def read_stream(cls, proc, stream, callback: Callable[[str], None]) -> None:
+    async def read_stream(cls, proc, stream, callback) -> None:
         """
         Reads the stout/stderr stream of a given process
         :param proc: the process to read the output from
@@ -47,7 +47,7 @@ class NodeProcessManager(object):
     @contextlib.asynccontextmanager
     async def open_process(self, cmd: str, *cmd_args: dict[str, str], work_dir: str = None,
                            env_vars: dict[str, str] = None, daemon: bool = False,
-                           stdout_callback: Callable[[str], None] = print) -> None:
+                           stdout_callback=print) -> None:
         """
         Creates a new Node process
 
@@ -91,7 +91,7 @@ class NodeProcessManager(object):
                     pass
 
     async def execute(self, js_file: str = None, port: int = None,
-                      stdout_callback: Callable[[str], None] = partial(print, flush=True)) -> None:
+                      stdout_callback=partial(print, flush=True)) -> None:
         """
         Use Node.js to run the provided script. If a port is given,
         the script will be run as a daemon
